d660b3cc5a4067fb65171322d2e1f0be92d20917
Anette Rivera <anettem.rivera@gmail.com>
2020-08-06 02:59:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+// const names = [`Lisa`, `Kaitlin`, `Jan` ]
+// const events = 'surprise'
+
+
+function writeCards(names, events){
+  let messages = [];
+  for (let i = 0; i< names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+  }
+  return messages;
+}
+  
+  writeCards(names, events)
+
